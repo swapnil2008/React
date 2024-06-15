122ca40c373e4419d2fe4a52d8b1130bce82c2c9
swapnil2008 <swapnilrgcg@gmail.com>
2024-06-15 17:03:18 +0530

    added tailwind css

---
+++ MyVite-ReactProject/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  
+return (
+    <>
+    <h1>Hello WORLD. This is My React App</h1>
+    <br />
+    <br />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
